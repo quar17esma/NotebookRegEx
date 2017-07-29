3b938d4e2f945860399e55d5a60fb8aa5db9f435
quar17esma <quar17esma@gmail.com>
2017-07-29 19:32:20 +0300

    add Notebook

---
+++ src/main/java/com/sergii/shutyi/Model/entity/Notebook.java
@@ -0,0 +1,16 @@
+package com.sergii.shutyi.Model.entity;
+
+import java.util.ArrayList;
+
+public class Notebook {
+
+    private ArrayList<Note> notes;
+
+    public ArrayList<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+}
