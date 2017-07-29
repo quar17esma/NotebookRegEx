6ecac89fed924acc088493e4172aae27cc7490bd
quar17esma <quar17esma@gmail.com>
2017-07-29 15:18:04 +0300

    add test

--- src/test/java/com/sergii/shutyi/Model/ModelUtilTest.java
+++ src/test/java/com/sergii/shutyi/Model/ModelUtilTest.java
@@ -1,5 +1,60 @@
+package com.sergii.shutyi.Model;
+
+import com.sergii.shutyi.Controller.InputUtil;
+import com.sergii.shutyi.Model.entity.Note;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ModelUtilTest {
+    @Test
+    public void setAllData() throws Exception {
+
+        Note noteExpected = new Note();
+        noteExpected.setLastName("John");
+        noteExpected.setFirstName("Smith");
+        noteExpected.setMiddleName("Arnold");
+        noteExpected.setLastNameInitials("John S.");
+        noteExpected.setNickName("syllas");
+        noteExpected.setCommentary("Test Note");
+        noteExpected.setGroup(Group.FAMILY);
+        noteExpected.setPhoneNumberHome("0444404545");
+        noteExpected.setPhoneNumberMobile("0968887979");
+        noteExpected.setPhoneNumberMobileAlt("0968887878");
+        noteExpected.setEmail("john@gmail.com");
+        noteExpected.setSkype("john");
+        noteExpected.setPostIndex("02105");
+        noteExpected.setCity("Kiev");
+        noteExpected.setStreet("Tampere");
+        noteExpected.setHouseNumber("88");
+        noteExpected.setApartmentNumber("44");
+        noteExpected.setFullAddress("02105, Kiev, Tampere, 88, 44");
+
+        Note noteActual = new Note();
+
+        InputUtil inputUtil = new InputUtil();
+        inputUtil.setLastName("John");
+        inputUtil.setFirstName("Smith");
+        inputUtil.setMiddleName("Arnold");
+        inputUtil.setNickName("syllas");
+        inputUtil.setCommentary("Test Note");
+        inputUtil.setGroup(Group.FAMILY);
+        inputUtil.setPhoneNumberHome("0444404545");
+        inputUtil.setPhoneNumberMobile("0968887979");
+        inputUtil.setPhoneNumberMobileAlt("0968887878");
+        inputUtil.setEmail("john@gmail.com");
+        inputUtil.setSkype("john");
+        inputUtil.setPostIndex("02105");
+        inputUtil.setCity("Kiev");
+        inputUtil.setStreet("Tampere");
+        inputUtil.setHouseNumber("88");
+        inputUtil.setApartmentNumber("44");
+
+        ModelUtil modelUtil = new ModelUtil();
+        modelUtil.setAllData(noteActual, inputUtil);
+
+        Assert.assertEquals(noteExpected, noteActual);
+    }
 
 }
