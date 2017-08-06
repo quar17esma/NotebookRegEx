d892ba36fe672723a9d4eed2b908c3085a17a292
quar17esma <quar17esma@gmail.com>
2017-08-06 16:50:05 +0300

    add Controller for Web

---
+++ src/main/java/com/sergii/shutyi/Controller/Controller.java
@@ -0,0 +1,24 @@
+package com.sergii.shutyi.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response){}
+}
