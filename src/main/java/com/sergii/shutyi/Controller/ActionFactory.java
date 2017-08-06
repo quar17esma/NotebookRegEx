20a564bb0ee25a9aba78f6ccbed35918a645c36b
quar17esma <quar17esma@gmail.com>
2017-08-06 22:46:41 +0300

    Action Factory

--- src/main/java/com/sergii/shutyi/Controller/ActionFactory.java
+++ src/main/java/com/sergii/shutyi/Controller/ActionFactory.java
@@ -1,4 +1,21 @@
 package com.sergii.shutyi.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class ActionFactory {
+    public ActionCommand defineCommand(HttpServletRequest request){
+        ActionCommand currentCommand = new EmptyCommand();
+
+        String action = request.getParameter("command");
+        if (action == null || action.isEmpty()) {
+            return currentCommand;
+        }
+        try {
+            CommandEnum currentEnum = CommandEnum.valueOf(action.toUpperCase());
+            currentCommand = currentEnum.getCurrentCommand();
+        } catch (IllegalArgumentException e) {
+            request.setAttribute("wrong action", action + MessageManager.getProperty("message.wrongaction"));
+        }
+        return currentCommand;
+    }
 }
