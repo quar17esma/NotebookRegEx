6abb398a468a794e33558f2ac3c52f458a114f8c
quar17esma <quar17esma@gmail.com>
2017-08-07 15:50:00 +0300

    add realization

--- src/main/java/com/sergii/shutyi/controller/command/ShowNotebookCommand.java
+++ src/main/java/com/sergii/shutyi/controller/command/ShowNotebookCommand.java
@@ -1,10 +1,17 @@
 package com.sergii.shutyi.controller.command;
 
+import com.sergii.shutyi.controller.manager.ConfigurationManager;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class ShowNotebookCommand implements ActionCommand {
     @Override
     public String execute(HttpServletRequest request) {
-        return null;
+        String page = null;
+
+//        request.setAttribute();
+
+        page = ConfigurationManager.getProperty("path.page.show.notebook");
+        return page;
     }
 }
